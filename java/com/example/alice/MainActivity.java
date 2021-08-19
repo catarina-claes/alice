22eeba6449c83070382b551d217606d303b552cf
noue <54459555+noue@users.noreply.github.com>
2021-08-19 13:46:49 +0700

    Add files via upload

---
+++ java/com/example/alice/MainActivity.java
@@ -0,0 +1,60 @@
+package com.example.alice;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Build;
+import android.os.Bundle;
+import android.provider.Settings;
+import android.view.View;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    private Activity context;
+
+    @Override
+    protected void onCreate(Bundle SavedInstanceSaved) {
+        super.onCreate(SavedInstanceSaved);
+        context = this;
+
+        this.setContentView(R.layout.activity_main);
+
+        this.findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (Alice.active == false) {
+                    startService();
+                } else
+                    stopService(new Intent(context, Alice.class));
+            }
+        });
+
+        checkPermission();
+        startService();
+    }
+
+    public void checkPermission() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (!Settings.canDrawOverlays(this)) {
+                Intent intent = new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION);
+                startActivity(intent);
+            }
+        }
+    }
+
+    public void startService() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (Settings.canDrawOverlays(this)) {
+                if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                    startForegroundService(new Intent(this, Alice.class));
+                } else {
+                    startService(new Intent(this, Alice.class));
+                }
+            }
+        } else {
+            startService(new Intent(this, Alice.class));
+        }
+    }
+
+}
